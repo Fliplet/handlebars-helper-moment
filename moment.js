494b0a42bb4393ea334885b98c294598588ac5f8
Tony Wu <tonytlwu@users.noreply.github.com>
2018-09-21 11:20:01 -0700

    Adds support for inputFormat and separates the display functions

--- moment.js
+++ moment.js
@@ -20,31 +20,71 @@ module.exports.register = function (Handlebars, options, params) {
       block = _.cloneDeep(context);
       context = undefined;
     }
+
     var date = moment(context);
-    
+
+    if (block.hash.inputFormat) {
+      date = moment(context, block.hash.inputFormat);
+    }
+
     if (block.hash.timezone){
       date.tz(block.hash.timezone);
     }
 
-    var hasFormat = false;
+    // Array of parameters to ignore
+    var ignoreParameters = [
+      'inputFormat',
+      'referenceTime',
+      'ignoreFromNowSuffix',
+      'ignoreToNowSuffix'
+    ];
+
+    // Array of display function to support, in priority order
+    var displayFunctions = [
+      'calendar',
+      'fromNow',
+      'toNow',
+      'format'
+    ];
 
     // Reset the language back to default before doing anything else
     date.lang('en');
 
     for (var i in block.hash) {
-      if (i === 'format') {
-        hasFormat = true;
-      }
-      else if (date[i]) {
+      if (_.includes(ignoreParameters, i) || _.includes(displayFunctions, i)) {
+        continue;
+      } else if (typeof date[i] === 'function') {
         date = date[i](block.hash[i]);
       } else {
         console.log('moment.js does not support "' + i + '"');
       }
     }
 
-    if (hasFormat) {
-      date = date.format(block.hash.format);
-    }
+    _.some(displayFunctions, function (func) {
+      if (!block.hash.hasOwnProperty(func)) {
+        return false;
+      }
+
+      switch (func) {
+        // Support for special parameters and syntax in display functions
+        case 'calendar':
+          date = date.calendar(block.hash.referenceTime);
+          break;
+        case 'fromNow':
+          date = date.fromNow(block.hash.ignoreFromNowSuffix);
+          break;
+        case 'toNow':
+          date = date.toNow(block.hash.ignoreToNowSuffix);
+          break;
+        default:
+          if (typeof date[func] === 'function') {
+            date = date[func](block.hash[func]);        
+          }
+          break;
+      }
+      return true;
+    });
+
     return date;
   });
 
